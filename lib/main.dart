ddaa4dd8d2025103f2a380d81feb90cb25e4d3e8
baps <rathod.kishan7up@gmail.com>
2021-03-17 18:36:50 +0530

    ch 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:learn_flutter_with_git/pages/home_page.dart';
+import 'package:learn_flutter_with_git/pages/login_page.dart';
 
 void main() {
   runApp(
     MaterialApp(
       title: "Awesome App",
-      home: HomePage(),
+      home: LoginPage(),
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
+      routes: {
+        "/login": (context) => LoginPage(),
+        "/home": (context) => HomePage()
+      },
     ),
   );
 }
