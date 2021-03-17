a64fc089b78e23f4fae91d21337187f6d2adf540
baps <rathod.kishan7up@gmail.com>
2021-03-17 18:08:21 +0530

    ch 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter_with_git/pages/home_page.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Awesome App",
-    home: HomePage(),
-  ));
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Awesome App"),
+  runApp(
+    MaterialApp(
+      title: "Awesome App",
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
       ),
-      body: Container(
-        child: Center(child: Text("Hi Flutter")),
-      ),
-    );
-  }
+    ),
+  );
 }
